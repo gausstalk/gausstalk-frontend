729f93e1bef867966090cc6bc6f8649d3e9a273e
Choi Yeonung <yonsweng@gmail.com>
2022-05-09 02:53:07 +0900

    Create anonymous websocket chat

--- src/index.js
+++ src/index.js
@@ -4,14 +4,43 @@ import './index.css';
 
 
 class Chat extends React.Component {
+  ws;
+
+  constructor(props) {
+    super(props);
+    this.ws = new WebSocket("ws://20.196.232.209/api/ws");
+    this.ws.onopen = () => {
+      console.log('conencted!!');
+    };
+    this.ws.onmessage = function(event) {
+      var chatArea = document.getElementById('chat-area-ul');
+      var message = document.createElement('li');
+      var content = document.createTextNode(event.data);
+      message.appendChild(content);
+      chatArea.appendChild(message);
+    };
+  }
+
+  sendMessage = (event) => {
+    var input = document.getElementById("chat-input");
+    this.ws.send(input.value);
+    input.value = '';
+    event.preventDefault();
+  };
+
   render() {
     return (
       <div className='chat-frame'>
-        <textarea className='chat-area' rows='8' readonly>
-          {'안녕\n마난서 반가워'}
-        </textarea>
-        <input type='chat-input' className='chat-input'/>
-        <button className='send-button'>Send</button>
+        <div className='chat-area'>
+          <ul id='chat-area-ul'>
+            <li>안녕</li>
+            <li>만나서 반가워</li>
+          </ul>
+        </div>
+        <form action='' onSubmit={this.sendMessage}>
+          <input className='chat-input' id='chat-input' type='text'/>
+          <button className='send-button'>Send</button>
+        </form>
       </div>
     );
   }
