9daf6e2631ea063cfe83691e0e154f8e1d39d52f
Choi Yeonung <yonsweng@gmail.com>
2022-05-20 00:45:46 +0900

    save access token to global variable and refresh token to cookie

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,28 +1,38 @@
-import { useSearchParams } from 'react-router-dom';
+import { useContext } from 'react';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { TokenContext } from './token-context.tsx';
 
 
 const Auth = () => {
   const [searchParams, ] = useSearchParams();
   const code = searchParams.get('code');
   const sessionState = searchParams.get('session_state');
+  const { token, setToken } = useContext(TokenContext);
+  const navigate = useNavigate();
 
-  // axios.post('http://localhost:8000/apps/user/v1/auth/', {
-  axios.post('https://talk.gausslabs.ai/api/apps/user/v1/auth/', {
-    code: code,
-    session_state: sessionState,
-  })
-  .then(function (response) {
-    let loginMessage = document.getElementById('login-message');
-    loginMessage.innerHTML = response.data;
-  }).catch(function (error) {
-    // error handling
-  });
+  if(token === null) {
+    // axios.post('http://localhost:8000/apps/user/v1/auth/', {
+    axios.post('https://talk.gausslabs.ai/api/apps/user/v1/auth/', {
+      code: code,
+      session_state: sessionState,
+    }, {
+      withCredentials: true,
+    })
+    .then(function (response) {
+      let loginMessage = document.getElementById('login-message');
+      loginMessage.innerHTML = response.data;
+      let gaussAccessToken = response.data['gauss_access_token'];
+      setToken(gaussAccessToken);
+      navigate("/chat");
+    }).catch(function (error) {
+      // error handling
+    });
+  }
 
   return (
     <div id='login-message'>Login...</div>
   );
 }
-
 
 export default Auth;
--- src/index.js
+++ src/index.js
@@ -7,19 +7,23 @@ import {
  } from 'react-router-dom';
 import Chat from './components/chat.js'
 import Auth from './components/auth.js'
-
+import { TokenProvider } from './components/token-context.tsx';
 import 'bootstrap/dist/css/bootstrap.css'
 import './assets/styles/index.css';
 
 
-const Root = () => (
-  <Router>
-    <Routes>
-      <Route exact path="/chat" element={<Chat />} />
-      <Route exact path="/auth" element={<Auth />} />
-    </Routes>
-  </Router>
-);
+const Root = () => {
+  return (
+    <Router>
+      <TokenProvider>
+        <Routes>
+            <Route exact path="/chat" element={<Chat />} />
+            <Route exact path="/auth" element={<Auth />} />
+        </Routes>
+      </TokenProvider>
+    </Router>
+  );
+};
 
 
 ReactDOM.render(
