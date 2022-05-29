c82006f1f491e3dbf8de6b742c2d836ea3b08465
Choi Yeonung <yonsweng@gmail.com>
2022-05-29 17:52:27 +0900

    load urls from environment variables

--- src/components/logout.js
+++ src/components/logout.js
@@ -1,6 +1,7 @@
 import { useContext, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import urlJoin from 'url-join';
 import { TokenContext } from './token-context.tsx';
 
 
@@ -9,8 +10,8 @@ const Logout = () => {
   const navigate = useNavigate();
 
   // delete gauss_refresh_token from cookie
-  //axios.delete('http://localhost:8000/apps/user/v1/auth/', {
-  axios.delete('https://talk.gausslabs.ai/api/apps/user/v1/auth/', {
+  const authUrl = urlJoin(process.env.REACT_APP_BACKEND_BASE_URL, 'apps/user/v1/auth/');
+  axios.delete(authUrl, {
     withCredentials: true,
   }).then(function (response) {
     console.log('Logout succeeded!!');
