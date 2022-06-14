226cfa4cbf1163009869d3d52ea3e872039196f5
YoohaBae <yooha.bae.2@gmail.com>
2022-06-14 14:02:56 +0900

    add register button

--- src/components/one-on-one.js
+++ src/components/one-on-one.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Button} from "@mui/material";
 import CustomNavbar from "./custom-nav-bar";
 import {WorkingFlipDate} from './countdown'
 import '../assets/styles/one-on-one.css'
+
+import { SnackbarProvider, useSnackbar} from 'notistack';
 
 
 const getTodayMidnight = () => {
@@ -13,10 +15,34 @@ const getTodayMidnight = () => {
     return tomorrow.toISOString();
 }
 
+const MyButton = () => {
+    //TODO: get register status and then useState
+    const [registered, setRegistered] = useState(false);
+    const { enqueueSnackbar, closeSnackbar } = useSnackbar();
+    const register = () => {
+        let success = "success";
+        let message = "Successfully registered"
+        enqueueSnackbar(message, {
+            variant: success
+        });
+        setRegistered(true);
+    };
+
+    //TODO: get registered status
+
+    if (!registered) {
+        return <Button variant="contained" onClick={register}>Register</Button>
+    }
+    else {
+        return <Button variant={"contained"} disabled>You have already registered! <br></br>Come back tomorrow</Button>
+    }
+}
+
 
 function OneOnOne() {
     return (<>
         <CustomNavbar />
+            <SnackbarProvider maxSnack={3}>
             <section className={"section"} id={"one-on-one-section"}>
                 <div className={"section-solid"}>
                     <div className={"section-content content"}>
@@ -25,7 +51,7 @@ function OneOnOne() {
                             <p>
                                 Every end of the day,<br></br>random 1:1’s are matched and sent to you via email!
                             </p>
-                            <Button variant="contained">Register</Button>
+                            <MyButton></MyButton>
                         </div>
                         <div className={"countdown-container"}>
                             <WorkingFlipDate value={getTodayMidnight()} />
@@ -34,6 +60,7 @@ function OneOnOne() {
                     </div>
                 </div>
             </section>
+        </SnackbarProvider>
         </>
     );
 }
