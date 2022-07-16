8d92803257b32915a242f0e455784abb6f233bc9
Choi Yeonung <yonsweng@gmail.com>
2022-07-16 19:05:02 +0900

    upgrade /lunch-together/form page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route
- } from 'react-router-dom';
+    BrowserRouter as Router,
+    Routes,
+    Route,
+} from 'react-router-dom';
 import Chat from './pages/chat.js';
 import Auth from './pages/auth.js';
 import Start from './pages/start.js';
@@ -13,6 +13,7 @@ import Signup from './pages/signup.js';
 import CreateUser from './pages/create-user.js';
 import OneOnOne from './pages/one-on-one.js'
 import Construction from './components/construction.js'
+import LunchTogether from './pages/lunch-together.js';
 import LunchTogetherForm from './pages/lunch-together-form.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/styles/index.css';
@@ -21,16 +22,18 @@ import './assets/styles/index.css';
 const Root = () => (
     <Router>
         <Routes>
-            <Route exact path={"/"} element={<Start/>}/>
+            <Route exact path="/" element={<Start />} />
             <Route exact path="/chat" element={<Chat />} />
             <Route exact path="/auth" element={<Auth />} />
             <Route exact path="/logout" element={<Logout />} />
-            <Route exact path="/contact" element={<Construction/>} />
+            <Route exact path="/contact" element={<Construction />} />
             <Route exact path="/signup" element={<Signup />} />
-            <Route exact path="/one-on-one" element={<OneOnOne/>}></Route>
+            <Route exact path="/one-on-one" element={<OneOnOne />}></Route>
             <Route exact path="/signup/create-user" element={<CreateUser />} />
-            <Route exact path={"/lunch-together"} element={<Construction/>}/>
-            <Route exact path="/lunch-together/form" element={<LunchTogetherForm/>}/>
+            <Route path="/lunch-together" element={<LunchTogether />}>
+                <Route path="" element={<Construction />} />
+                <Route path="form" element={<LunchTogetherForm />} />
+            </Route>
         </Routes>
     </Router>
 );
