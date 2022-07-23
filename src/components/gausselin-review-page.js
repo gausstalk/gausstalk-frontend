7601ffbc674743aad332a18c6877c07569020534
Choi Yeonung <yonsweng@gmail.com>
2022-07-23 23:32:26 +0900

    create gausselin review page

---
+++ src/components/gausselin-review-page.js
@@ -0,0 +1,83 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import urlJoin from 'url-join';
+import { Paper } from '@mui/material';
+import StarIcon from '@mui/icons-material/Star';
+import StarBorderIcon from '@mui/icons-material/StarBorder';
+import '../assets/styles/gausselin.css';
+
+
+const GausselinReviewPage = ({ offset, limit }) => {
+  const [reviews, setReviews] = useState();
+  const [reviewPapers, setReviewPapers] = useState();
+  const token = window.sessionStorage.getItem('gaussAccessToken');
+  const reviewsUrl = urlJoin(process.env.REACT_APP_BACKEND_BASE_URL, 'apps/gausselin/v1/reviews/');
+
+  // Get reviews.
+  useEffect(() => {
+    setReviewPapers('Loading...');
+
+    axios.get(reviewsUrl, {
+      params: { offset: offset, limit: limit },
+      headers: { Authorization: `Bearer ${token}` },
+      withCredentials: true,
+    }).then((response) => {
+      setReviews(response.data);
+    }).catch((error) => {
+      console.log(error);
+    });
+  }, [token, reviewsUrl, offset, limit]);
+
+  // Show reviews.
+  useEffect(() => {
+    if (reviews) {
+      let tempReviewPapers = [];
+
+      reviews.forEach(review => {
+        // Draw stars.
+        let starIcons = [];
+        for (let i = 0; i < review.stars; i++) {
+          starIcons.push(<StarIcon sx={{ color: 'orange' }} key={i} />);
+        }
+        for (let i = review.stars; i < 5; i++) {
+          starIcons.push(<StarBorderIcon sx={{ color: 'orange' }} key={i} />);
+        }
+
+        tempReviewPapers.push(
+          <Paper elevation={3} className='gausselin-paper' key={review.id}>
+            <table>
+              <tbody>
+                <tr>
+                  <td>Restaurant</td>
+                  <td><a href={`https://place.map.kakao.com/${review.restaurant_id}`}>
+                    {review.restaurant_name}
+                  </a></td>
+                </tr>
+                <tr>
+                  <td>Reviewer</td>
+                  <td>{review.user_name}</td>
+                </tr>
+                <tr>
+                  <td>Stars</td>
+                  <td>{starIcons}</td>
+                </tr>
+                <tr>
+                  <td>Comment</td>
+                  <td>{review.comment}</td>
+                </tr>
+              </tbody>
+            </table>
+          </Paper>
+        );
+      });
+
+      setReviewPapers(tempReviewPapers);
+    }
+  }, [reviews]);
+
+  return (
+    <>{reviewPapers}</>
+  );
+};
+
+export default GausselinReviewPage;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import {
-    BrowserRouter as Router,
-    Routes,
-    Route,
+  BrowserRouter as Router,
+  Routes,
+  Route,
 } from 'react-router-dom';
 import Chat from './pages/chat.js';
 import Auth from './pages/auth.js';
@@ -15,27 +15,29 @@ import OneOnOne from './pages/one-on-one.js'
 import Construction from './components/construction.js'
 import LunchTogether from './pages/lunch-together.js';
 import LunchTogetherForm from './pages/lunch-together-form.js';
+import Gausselin from './pages/gausselin.js';
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/styles/index.css';
 
 
 const Root = () => (
-    <Router>
-        <Routes>
-            <Route exact path="/" element={<Start />} />
-            <Route exact path="/chat" element={<Chat />} />
-            <Route exact path="/auth" element={<Auth />} />
-            <Route exact path="/logout" element={<Logout />} />
-            <Route exact path="/contact" element={<Construction />} />
-            <Route exact path="/signup" element={<Signup />} />
-            <Route exact path="/one-on-one" element={<OneOnOne />}></Route>
-            <Route exact path="/signup/create-user" element={<CreateUser />} />
-            <Route path="/lunch-together" element={<LunchTogether />}>
-                <Route path="" element={<Construction />} />
-                <Route path="form" element={<LunchTogetherForm />} />
-            </Route>
-        </Routes>
-    </Router>
+  <Router>
+    <Routes>
+      <Route exact path="/" element={<Start />} />
+      <Route exact path="/chat" element={<Chat />} />
+      <Route exact path="/auth" element={<Auth />} />
+      <Route exact path="/logout" element={<Logout />} />
+      <Route exact path="/contact" element={<Construction />} />
+      <Route exact path="/signup" element={<Signup />} />
+      <Route exact path="/one-on-one" element={<OneOnOne />}></Route>
+      <Route exact path="/signup/create-user" element={<CreateUser />} />
+      <Route path="/lunch-together" element={<LunchTogether />}>
+        <Route path="" element={<Construction />} />
+        <Route path="form" element={<LunchTogetherForm />} />
+      </Route>
+      <Route exact path="/gausselin" element={<Gausselin />} />
+    </Routes>
+  </Router>
 );
 
 
