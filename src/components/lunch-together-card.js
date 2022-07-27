1123fd38b5dbaf3d06e2e1ceabd536f9654dcd4c
YoohaBae <yooha.bae.2@gmail.com>
2022-07-27 00:45:41 -0700

    connect to backend

--- src/components/lunch-together-card.js
+++ src/components/lunch-together-card.js
@@ -4,12 +4,28 @@ import { green } from '@mui/material/colors';
 
 import LunchTogetherModal from './lunch-together-modal.js'
 import '../assets/styles/lunch-together-card.css'
+import axios from "axios";
+import urlJoin from "url-join";
 
-export default function MultiActionAreaCard(restaurant, date, organizer, participants, meeting_point) {
+export default function MultiActionAreaCard({
+    appointment_id,
+    datetime,
+    meeting_point,
+    n_participants,
+    organizer_mail,
+    organizer_name,
+    restaurant_id,
+    title
+}) {
+    const token = window.sessionStorage.getItem('gaussAccessToken');
     const [open, setOpen] = React.useState(false);
     const [loading, setLoading] = React.useState(false);
     const [success, setSuccess] = React.useState(false);
     const timer = React.useRef();
+    const formattedDatetime = new Date(datetime);
+    const today = new Date()
+    let difference = formattedDatetime.getTime() - today.getTime();
+    let daysUntil = "D-" + Math.ceil(difference / (1000 * 3600 * 24));
 
     const buttonSx = {
         ...(success && {
@@ -21,10 +37,28 @@ export default function MultiActionAreaCard(restaurant, date, organizer, partici
     };
 
     React.useEffect(() => {
+        async function fetchData() {
+            // retrieve the first 6 appointments
+            return await axios.get(urlJoin(process.env.REACT_APP_BACKEND_BASE_URL, 'apps/lunch-together/v1/registrations/'), {
+                headers: {Authorization: `Bearer ${token}`},
+                withCredentials: true,
+                params: {"appointment_id": appointment_id}
+            })
+        }
+
+        fetchData().then((res) => {
+            if (res.status === 200) {
+                setSuccess(true);
+                setLoading(false);
+            } else {
+                setSuccess(false);
+                setLoading(false);
+            }
+        });
         return () => {
             clearTimeout(timer.current);
         };
-    }, []);
+    }, [appointment_id, token]);
 
     const handleOpenModal = () => {
         setOpen(true);
@@ -39,8 +73,25 @@ export default function MultiActionAreaCard(restaurant, date, organizer, partici
             setSuccess(false);
             setLoading(true);
             timer.current = window.setTimeout(() => {
-                setSuccess(true);
-                setLoading(false);
+                async function fetchData() {
+                    // retrieve the first 6 appointments
+                    return await axios.put(urlJoin(process.env.REACT_APP_BACKEND_BASE_URL, 'apps/lunch-together/v1/registrations/'), {}, {
+                        headers: {Authorization: `Bearer ${token}`},
+                        withCredentials: true,
+                        params: {"appointment_id": appointment_id}
+                    })
+                }
+
+                fetchData().then((res) => {
+                    if (res.status === 200) {
+                        setSuccess(true);
+                        setLoading(false);
+                    } else {
+                        setSuccess(false);
+                        setLoading(false);
+                    }
+                });
+
             }, 2000);
         }
     };
@@ -57,18 +108,18 @@ export default function MultiActionAreaCard(restaurant, date, organizer, partici
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         <div className={"align-vertical-center"}>
-                            대우부대찌개
-                            <Chip className={"chip"} label="D-1" color="info" size={"small"} />
+                            {title}
+                            <Chip className={"chip"} label={daysUntil} color="info" size={"small"} />
                         </div>
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        <span className={"date"}>Aug 21st 1:00pm ~ 2:00pm</span>
+                        <span className={"date"}>{datetime}</span>
                         <br/>
-                        <span className={"planner"}>Organizer: Yooha Bae</span>
+                        <span className={"planner"}>Organizer: {organizer_name}</span>
                         <br/>
-                        <span className={"current-count"}>Participants: 3/10</span>
+                        <span className={"current-count"}>Participants: {n_participants}/10</span>
                         <br/>
-                        <span className={"meeting-place"}>Meeting point: Lobby</span>
+                        <span className={"meeting-place"}>Meeting point: {meeting_point}</span>
                         <br/>
                     </Typography>
                 </CardContent>
